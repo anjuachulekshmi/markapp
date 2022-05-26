6f2b2d84f30bc6da24ac066ed55512865bc8982a
anjuachulekshmi <anjuachulekshmi836@gmail.com>
2022-05-26 06:44:30 +0530

    student codded

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import  'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Student from './Component/Student';
+import View from './Component/View';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
    <div>
-     <Student />
+     <BrowserRouter>
+     <Routes>
+       <Route path="/" exact element={<Student/>}/>
+       <Route path="/View" exact element={<View/>}/>
+     </Routes>
+     </BrowserRouter>
    </div>
   );
 }
--- src/Component/Student.js
+++ src/Component/Student.js
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Header from './Header'
 
 const Student = () => {
+    var [name,setname]=useState("")
+    var [admnno,setadmnno]=useState("")
+    var [cgpa,setcgpa]=useState("")
+    const setData=()=>{
+        var data={"name":name,"addmnno":admnno,"cgpa":cgpa}
+        console.log(data)
+}
   return (
     <div>
+        <Header/>
         <div className="container">
     <div className="row">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
             <div className="row g-3">
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Name</label>
-                   <input type="text" className="form-control"/>
+                   <input onChange={(e)=>setname(e.target.value)}  type="text" className="form-control"/>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">Admission Number</label>
-                    <input type="text" className="form-control"/>
+                    <input onChange={(e)=>setadmnno(e.target.value)} type="text" className="form-control"/>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label for="" className="form-label">CGPA</label>
-                    <input type="text" className="form-control"/>
+                    <input onChange={(e)=>setcgpa(e.target.value)} type="text" className="form-control"/>
                 </div>
-                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                <button className="btn btn-success">register</button>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <button onClick={setData} className="btn btn-success">register</button>
                 </div>
                
                 </div>
