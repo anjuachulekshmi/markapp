095252f638a108e6d45793b63840500ddf9b0d4b
anjuachulekshmi <anjuachulekshmi836@gmail.com>
2022-05-26 06:01:42 +0530

    student codded

---
+++ src/Component/Student.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Student = () => {
+  return (
+    <div>
+        <div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">Name</label>
+                   <input type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">Admission Number</label>
+                    <input type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">CGPA</label>
+                    <input type="text" className="form-control"/>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <button className="btn btn-success">register</button>
+                </div>
+               
+                </div>
+                </div>
+                </div>
+                </div>
+    </div>
+  )
+}
+
+export default Student
